b99231f789b907c7afa303bbeb0e37b885aa93f3
eduardorcury <eduardoribeirocury@gmail.com>
2021-04-23 16:42:43 -0300

    Correção build

--- src/test/java/com/zup/academy/edurardoribeiro/Proposta/criacao/CriacaoPropostaControllerTest.java
+++ src/test/java/com/zup/academy/edurardoribeiro/Proposta/criacao/CriacaoPropostaControllerTest.java
@@ -1,6 +1,8 @@
 package com.zup.academy.edurardoribeiro.Proposta.criacao;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zup.academy.edurardoribeiro.Proposta.analise.AnaliseFinanceiraClient;
+import com.zup.academy.edurardoribeiro.Proposta.analise.AnaliseFinanceiraService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,6 +10,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -30,6 +33,9 @@ class CriacaoPropostaControllerTest {
 
     @Autowired
     ObjectMapper mapper;
+
+    @MockBean
+    AnaliseFinanceiraService service;
 
     @Test
     @DisplayName("Criação de proposta válida")
